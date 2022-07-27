6730daf04ebf03ca12e82afe3f2b90980d7f3919
Sferas379 <rthyssen73@gmail.com>
2022-07-27 15:40:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const myCity = {
+  city: 'New-York',
+  country: "USA"
+}
+let a = JSON.stringify(myCity)
+console.log(JSON.parse(a ));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Hallo</h1>
+
+       
     </div>
   );
 }
