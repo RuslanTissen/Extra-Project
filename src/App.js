3244ce281030d90fe7a6d403c5ac7fd918736d8b
Sferas379 <rthyssen73@gmail.com>
2022-07-31 20:30:46 +0200

    just for refresh my JS knowledge

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 
+
 function App() {
- function myFn(a,b){
-  let c
-  a=a+1
-  c = a+b
-  return c
- }
- console.dir(myFn.name);
- let d = myFn(14,25)
- console.log(d);
+  'use strict'
+
+  function myFn (){
+    a = true
+    console.log(a);
+  }
+  myFn()
+  console.log(a);
 
   return (
     <div className="App">
