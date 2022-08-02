9b8a464096b5b5cf5b015a9f8cc17284bb98d278
Sferas379 <rthyssen73@gmail.com>
2022-08-02 10:46:45 +0200

    continue practise some JS 2.8.22

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,13 +1,23 @@
 import {NavLink} from "react-router-dom"
 import "./Header.scss"
+import Logo from "./components/Logo.png"
 
 export default function Header(){
+    const fnWithError = ()=> {
+        throw new Error('Some Error')            
+    }
+    try {
+        fnWithError()        
+    } catch (error) {
+        console.error(error)
+        console.log(error.message);
+    }
 
-
+    console.log('Continue...');
 
     return(
         <nav>
-            <NavLink to="*"><span className="logoNav">Ruslan's LOGO</span><img src="./components/Logo.png" id="logo" /></NavLink>
+            <NavLink to="*"><span className="logoNav">Ruslan's LOGO</span><img src={Logo} id="logo" /></NavLink>
 
         </nav>
     )
