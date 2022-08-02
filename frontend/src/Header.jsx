003d50f7912128aeb364d1d526a6cf1e1332f961
Sferas379 <rthyssen73@gmail.com>
2022-08-02 14:36:45 +0200

    added some links and css

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -2,6 +2,7 @@ import {NavLink} from "react-router-dom"
 import "./Header.scss"
 import Logo from "./components/Logo.png"
 
+
 export default function Header(){
     const fnWithError = ()=> {
         throw new Error('Some Error')            
@@ -16,9 +17,16 @@ export default function Header(){
     console.log('Continue...');
 
     return(
-        <nav>
-            <NavLink to="*"><span className="logoNav">Ruslan's LOGO</span><img src={Logo} id="logo" /></NavLink>
-
+        <header>
+            <nav>
+            <NavLink to="*">
+                {/* <span className="logoNav">Ruslan's LOGO</span> */}
+                <img src={Logo} id="logo" /></NavLink>
+            <NavLink to="Videos">Videos</NavLink>
+            <NavLink to="Forum">Forums</NavLink>
+            <NavLink to="Project">Project</NavLink>
         </nav>
+        </header>
+        
     )
 }
