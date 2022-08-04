fb55b15eecd6d31956e104a9aa6c037d1ef3ab15
Sferas379 <rthyssen73@gmail.com>
2022-08-04 10:54:35 +0200

    nothing 4.8.22

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,24 +1,30 @@
-import {NavLink} from "react-router-dom"
-import "./Header.scss"
-import Logo from "./components/Logo.png"
+import { NavLink } from "react-router-dom";
+import "./Header.scss";
+import Logo from "./components/Logo.png";
 
+export default function Header() {
+  const userProfile = {
+    name : "Ruslan",
+    age: 48,
+    hasSignedAgreement: true
+  }
 
-export default function Header(){
-    function myFn(a,b){return a+b}
-    // const a = myFn(2,3)
-    console.log(myFn(2,3));
+//   const {name, age} = userProfile
+// const {hasSignedAgreement} = userProfile
 
-    return(
-        <header>
-            <nav>
-            <NavLink to="*">
-                {/* <span className="logoNav">Ruslan's LOGO</span> */}
-                <img src={Logo} id="logo" /></NavLink>
-            <NavLink to="Videos">Videos</NavLink>
-            <NavLink to="Forum">Forums</NavLink>
-            <NavLink to="Project">Project</NavLink>
-        </nav>
-        </header>
-        
-    )
+console.log(userProfile.name);
+
+  return (
+    <header>
+      <nav>
+        <NavLink to="*">
+          {/* <span className="logoNav">Ruslan's LOGO</span> */}
+          <img src={Logo} id="logo" />
+        </NavLink>
+        <NavLink to="Videos">Videos</NavLink>
+        <NavLink to="Forum">Forums</NavLink>
+        <NavLink to="Project">Project</NavLink>
+      </nav>
+    </header>
+  );
 }
